9e9e8b2b4a204e20ae7859a1ecba8c82a083d7a5
Jabba Laci <jabba.laci@gmail.com>
2014-04-03 18:40:14 +0200

    modernization

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 import sys
 
+
 def main(name):
-    print "Hello, %s!" % name
+    print("Hello, {name}!".format(name=name))
+
+##############################################################################
 
 if __name__ == "__main__":
-    name = 'World'
+    name = "World"
     if len(sys.argv) > 1:
         name = sys.argv[1]
     main(name)
